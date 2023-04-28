155e5e8e4ac524077685b31ada2631f9bccb1222
“theoioannou225@gmail.com” <theoioannou225@gmail.com>
2023-04-28 11:54:36 +0100

    aboutUs, Register, Login

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
 const express = require('express');
 const app = express();
-
 const path = require('path');
 //const Database = require('nedb');
 const public = path.join(__dirname,'public');
-app.use(express.static(public));
+app.use(express.static('public'));
 
 //setting up the database
 //const db = {};
 //db.users = new Database 
 //const public = path.join(__dirname, 'public');
+
 const router = require('./routes/wellnessAppRoutes');
 app.use('/', router); 
 
-//app.get('/', function (req, res) {
- //   res.send('Hello! Welcome to my wellness web application.');
-//})
+app.get('/', function (req, res) {
+   res.send('Hello! Welcome to my wellness web application.');
+})
 
 app.get('/aboutUs', function (req, res) {
    res.sendFile(path.join(public, 'aboutUs.html'));
 })
 
 
-//app.get('/login', function (req, res) {
-//    res.sendFile(path.join(public, 'login.html'));
-//})
+app.get('/login', function (req, res) {
+    res.sendFile(path.join(public, 'login.html'));
+})
 
-//app.get('/register', function (req, res) {
- //   res.sendFile(path.join(public, 'register.html'));
-//})
+app.get('/register', function (req, res) {
+   res.sendFile(path.join(public, 'register.html'));
+})
 
-//app.use(function (req, res) {
- //   res.status(404);
- //   res.send('Oops! We didn\'t find what you are looking for.');
-//})
+app.use(function (req, res) {
+   res.status(404);
+    res.send('Oops! We didn\'t find what you are looking for.');
+})
 
 app.listen(3000, () => {
     console.log('Server started on port 3000. Ctrl^c to quit.');
