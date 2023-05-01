975dddeff7a8a515941f89b38ee9bd9d74500503
“theoioannou225@gmail.com” <theoioannou225@gmail.com>
2023-05-01 22:11:12 +0100

    did the functionality for the adding a new nutrition goal

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const sqlite3 = require('sqlite3').verbose();
 const express = require('express');
 const app = express();
 const mustache = require('mustache-express');
@@ -12,20 +13,58 @@ app.use(express.static('public'));
 
 
 
+let db = new sqlite3.Database('./database/nutritionGoals.db', sqlite3.OPEN_READWRITE,
+   (err) => {
+      if (err) {
+         console.error(err.message);
+      } else
+         console.log('Connected to the nutritionGoals database.');
+   });
 
-
-
-const router = require('./routes/wellnessAppRoutes');
-app.use('/', router);
-
-app.get('/', function (req, res) {
-   res.send('Hello! Welcome to my wellness web application.');
-})
+db.run('CREATE TABLE IF NOT EXISTS nGoals(id TEXT, maxKcal NUMBER, startDate DATE, endDate DATE)');
+app.get('/', function(req,res){
+   res.sendFile(path.join(__dirname,'./public/index.html'));
+ });
+// Insert
+app.post('/add', function (req, res) {
+   db.serialize(() => {
+      db.run('INSERT INTO nGoals(id,maxKcal,startDate,endDate) VALUES(?,?,?,?)', [req.body.id, req.body.maxKcal, req.body.startDate, req.body.endDate], function (err) {
+         if (err) {
+            return console.log(err.message);
+         }
+         console.log("New nutrition goal has been added");
+         res.sendFile(path.join(__dirname,'./public/index.html'));
+      });
+   });
+});
 
 app.get('/aboutUs.mustache', function (req, res) {
    res.sendFile(path.join(public, 'aboutUs.mustache'));
 })
 
+
+const router = require('./routes/wellnessAppRoutes');
+app.use('/', router);
+
+
+app.get('/', function (req, res) {
+   res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+
+
+app.post('/addNutritionGoals', function (req, res) {
+   db.serialize(() => {
+      db.run('INSERT INTO nutrinionGoals(id, maxKcal, startDate, endDate) VALUES(?,?,?,?)'[req.body.id, req.body.maxKcal, req.body.startDate, req.body.endDate],
+         function (err) {
+            if (err) {
+               return console.log(err.message);
+            }
+            console.log("New nutrition goal has been added");
+            res.sendFile(path.join(__dirname, './nutrition/nutrition.mustache'));
+         });
+   });
+});
 
 app.use(function (req, res) {
    res.status(404);
